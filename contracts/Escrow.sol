fcb3aa273a9ad9ec09fe2f34cc5e4b9f95a5564f
daryle <daryle.tan@gmail.com>
2023-07-09 18:45:37 -0500

    installed dependencies and added escrow.sol

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,27 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
 contract Escrow {
-	address public arbiter;
-	address public beneficiary;
-	address public depositor;
+    // depositor is signer
+    address public depositor;
+    // beneficiary
+    address public beneficiary;
+    // arbiter
+    address public arbiter;
+    bool public isApproved;
 
-	bool public isApproved;
+    constructor(address _arbiter, address _beneficiary) payable {
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+        depositor = msg.sender;
+        isApproved = false;
+    }
 
-	constructor(address _arbiter, address _beneficiary) payable {
-		arbiter = _arbiter;
-		beneficiary = _beneficiary;
-		depositor = msg.sender;
-	}
+    event Approved(uint);
 
-	event Approved(uint);
+    function approve() external {
+        uint balance = address(this).balance;
+        require(msg.sender == arbiter);
+        (bool sent, ) = beneficiary.call{value: balance}("");
+        require(sent, "Failed to send funds!");
+        isApproved = true;
 
-	function approve() external {
-		require(msg.sender == arbiter);
-		uint balance = address(this).balance;
-		(bool sent, ) = payable(beneficiary).call{value: balance}("");
- 		require(sent, "Failed to send Ether");
-		emit Approved(balance);
-		isApproved = true;
-	}
+        emit Approved(balance);
+    }
 }
